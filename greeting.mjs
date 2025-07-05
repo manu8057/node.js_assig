10f422dc7bb44c133b5c95af47117243985c2180
apnacollege <manupandit0022@gmail.com>
2025-07-05 10:11:38 +0530

    pehla commit

---
+++ greeting.mjs
@@ -0,0 +1,4 @@
+function greet(name){
+    console.log(`Hello, ${name}! Welcome to the Node.js module system.`);
+}
+export {greet};
---
+++ main.mjs
@@ -0,0 +1,7 @@
+import * as greeting from './greeting.mjs';
+import * as math from './mathOperations.mjs';
+greeting.greet("tanmay");
+console.log(math.sum(6,5));
+console.log(math.sub(6,5));
+console.log(math.mul(6,5));
+console.log(math.div(6,5));
